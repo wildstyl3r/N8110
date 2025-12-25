7085e3b5172ef65e6f66191c046fcaf37a024c02
Mike <wildstyl3r@yandex.ru>
2025-12-26 00:10:12 +0300

    fix checking debasing

--- script.js
+++ script.js
@@ -181,7 +181,7 @@ function decodeChunks(rawData, expectedType) {
             base64 += '='.repeat(padding);
             
             const decoded = atob(base64);
-            log(`debased successfully: ${decoded}`);
+            log(`debased successfully: ${decoded.slice(Math.max(arr.length - 5, 1))}`);
             const chunkData = JSON.parse(decoded);
             
             // Validate
