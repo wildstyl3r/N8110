3de458174dbeb7c5f6655fe1e864e50de4618201
Mike <wildstyl3r@yandex.ru>
2025-12-28 19:57:27 +0300

    fix circuit import

--- business.js
+++ business.js
@@ -1,9 +1,10 @@
+/* global  Libp2PCircuitRelayV2*/
 // Global state
 
 import { ui } from './ui.js';
 import pako from 'pako';
 import { createLibp2p } from 'libp2p';
-import { circuitRelayTransport } from '@libp2p/circuit-relay-v2';
+const { circuitRelayTransport } = Libp2PCircuitRelayV2;
 import { noise } from 'libp2p-noise';
 import { yamux } from 'libp2p-yamux';
 let dataPc = null;
