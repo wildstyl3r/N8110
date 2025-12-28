02e351bbea7c6c61e235f46757a2811d5cac7f2b
Mike <wildstyl3r@yandex.ru>
2025-12-28 04:14:39 +0300

    change prov

--- business.js
+++ business.js
@@ -6,8 +6,8 @@ let localStream = null;
 let dataChannel = null;
 const config = { 
     iceServers: [
-        {urls: 'stun:stun.l.google.com:19302'},
-        {urls: 'stun:stun1.l.google.com:19302'},
+        {urls: 'stun.sipnet.ru:3478'},
+        // {urls: 'stun:stun1.l.google.com:19302'},
         // {urls: 'stun:stun.arbuz.ru:3478'},
         // {urls: 'stun:stun.comtube.ru:3478'},
         // {urls: 'stun:stun.demos.ru:3478'},
